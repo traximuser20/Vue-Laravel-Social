3517f9ecfa8d9f91ef5804bba4713fc7c66e71d5
Traxim User <traximuser20@gmail.com>
2024-04-24 00:21:37 +0500

    --group-migrations-changed

--- database/migrations/2024_04_22_144454_create_groups_table.php
+++ database/migrations/2024_04_22_144454_create_groups_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name', 255);
             $table->string('slug', 255);
+            $table->string('cover_path', 1024)->nullable();
+            $table->string('cover_url', 1024)->nullable();
+            $table->string('thumbnail_path', 1024)->nullable();
             $table->boolean('auto_approval')->default(true);
             $table->text('about')->nullable();
             $table->foriegnId('user_id')->constrained('users');
